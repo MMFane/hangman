af6fb0dc5e462edc2da2ae238848639c830f68de
Sam <sam.williams@littlebytegames.com>
2021-10-09 22:46:05 -0700

    replace theme name strings with enum

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,19 @@
 let game1;
 const puzzleLength = "2";
 const guesses = 5;
+const themesEnum = ["light", "dark", "contrast", "halloween"];
+const urlsEnum = [
+  "./styles/theme-light.css",
+  "./styles/theme-dark.css",
+  "./styles/theme-contrast.css",
+  "./styles/theme-halloween.css",
+];
 
 const resetBtn = document.querySelector("#reset");
-const lightBtn = document.querySelector("#light");
-const darkBtn = document.querySelector("#dark");
-const contrastBtn = document.querySelector("#contrast");
-const halloweenBtn = document.querySelector("#halloween");
+const lightBtn = document.querySelector(`#${themesEnum[0]}`);
+const darkBtn = document.querySelector(`#${themesEnum[1]}`);
+const contrastBtn = document.querySelector(`#${themesEnum[2]}`);
+const halloweenBtn = document.querySelector(`#${themesEnum[3]}`);
 halloweenBtn.disabled = true;
 
 const themeButtons = [lightBtn, darkBtn, contrastBtn, halloweenBtn];
@@ -34,17 +41,17 @@ const resetThemeButtons = (flag) => {
 
 const changeTheme = (theme) => {
   resetThemeButtons(false);
-  if (theme === "light") {
-    changeLink("./styles/theme-light.css");
+  if (theme === themesEnum[0]) {
+    changeLink(`./styles/theme-${themesEnum[0]}.css`);
     lightBtn.disabled = true;
-  } else if (theme === "dark") {
-    changeLink("./styles/theme-dark.css");
+  } else if (theme === themesEnum[1]) {
+    changeLink(`./styles/theme-${themesEnum[1]}.css`);
     darkBtn.disabled = true;
-  } else if (theme === "contrast") {
-    changeLink("./styles/theme-contrast.css");
+  } else if (theme === themesEnum[2]) {
+    changeLink(`./styles/theme-${themesEnum[2]}.css`);
     contrastBtn.disabled = true;
-  } else if (theme === "halloween") {
-    changeLink("./styles/theme-halloween.css");
+  } else if (theme === themesEnum[3]) {
+    changeLink(`./styles/theme-${themesEnum[3]}.css`);
     halloweenBtn.disabled = true;
   } else {
     console.log("Error: unknown theme in changeTheme()");
@@ -53,9 +60,9 @@ const changeTheme = (theme) => {
 
 // set up buttons
 resetBtn.addEventListener("click", startGame);
-lightBtn.addEventListener("click", () => changeTheme("light"));
-darkBtn.addEventListener("click", () => changeTheme("dark"));
-contrastBtn.addEventListener("click", () => changeTheme("contrast"));
-halloweenBtn.addEventListener("click", () => changeTheme("halloween"));
+lightBtn.addEventListener("click", () => changeTheme(themesEnum[0]));
+darkBtn.addEventListener("click", () => changeTheme(themesEnum[1]));
+contrastBtn.addEventListener("click", () => changeTheme(themesEnum[2]));
+halloweenBtn.addEventListener("click", () => changeTheme(themesEnum[3]));
 
 startGame();
